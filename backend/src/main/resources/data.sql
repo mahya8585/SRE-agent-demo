13018fd85867f49de71fb489e38cafeb5d62ad1b
maaya <huki.j37@gmail.com>
2026-08-10 14:48:41 +0900

    データ類修正

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO wine (name, region, variety, vintage, category, image, price, stock, threshold) VALUES
-('Château Lueur Noire', 'Bordeaux', 'Cabernet Sauvignon', '2018', 'Red', '/assets/wines/chateau-lueur-noire.jpg', 148000.0, 24, 8),
-('Monteluna Estate', 'Tuscany', 'Sangiovese', '2019', 'Red', '/assets/wines/monteluna-estate.jpg', 92000.0, 19, 6),
-('Aotearoa Cellars', 'Marlborough', 'Sauvignon Blanc', '2021', 'White', '/assets/wines/aotearoa-cellars.jpg', 74000.0, 35, 10),
-('Valle di Sera', 'Piedmont', 'Nebbiolo', '2017', 'Red', '/assets/wines/valle-di-sera.jpg', 166000.0, 14, 5),
-('Moonlight Spark', 'Champagne', 'Champagne Blend', '2020', 'Sparkling', '/assets/wines/moonlight-spark.jpg', 118000.0, 22, 7),
-('Sakura Reserve', 'Yamanashi', 'Koshu', '2020', 'White', '/assets/wines/sakura-reserve.jpg', 86000.0, 12, 4);
+('Château Lueur Noire', 'Bordeaux', 'Cabernet Sauvignon', '2018', 'Red', '/assets/wines/chateau-lueur-noire.jpg', 7400.0, 24, 8),
+('Monteluna Estate', 'Tuscany', 'Sangiovese', '2019', 'Red', '/assets/wines/monteluna-estate.jpg', 4600.0, 19, 6),
+('Aotearoa Cellars', 'Marlborough', 'Sauvignon Blanc', '2021', 'White', '/assets/wines/aotearoa-cellars.jpg', 3700.0, 35, 10),
+('Valle di Sera', 'Piedmont', 'Nebbiolo', '2017', 'Red', '/assets/wines/valle-di-sera.jpg', 8300.0, 14, 5),
+('Moonlight Spark', 'Champagne', 'Champagne Blend', '2020', 'Sparkling', '/assets/wines/moonlight-spark.jpg', 5900.0, 22, 7),
+('Sakura Reserve', 'Yamanashi', 'Koshu', '2020', 'White', '/assets/wines/sakura-reserve.jpg', 4300.0, 12, 4);
